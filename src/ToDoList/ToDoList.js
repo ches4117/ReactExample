8d9d713bd2389def4dc15aa522c344b6f88ba975
ches4117 <ches4117@gmail.com>
2019-12-22 13:36:10 +0800

    Fix bug

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -36,7 +36,7 @@ export default function ToDoList() {
     });
     setTodos(newTodos);
     setTimeout(() => {
-      document.forms[0].elements[i + 1].focus();
+      document.forms && document.forms[0].elements[i + 1].focus();
     }, 0);
   }
 
@@ -50,7 +50,7 @@ export default function ToDoList() {
     if (i === 0 && todos.length === 1) return;
     setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
     setTimeout(() => {
-      document.forms[0].elements[i - 1].focus();
+      document.forms && document.forms[0].elements[i - 1] && document.forms[0].elements[i - 1].focus();
     }, 0);
   }
 
